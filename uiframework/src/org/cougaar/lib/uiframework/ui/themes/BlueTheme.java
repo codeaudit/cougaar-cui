8d044e152369d0c0393e0699d56aa45eb83f9ba0
pfischer <pfischer>
2001-02-07 16:38:06 +0000

    Modified this theme to use delta colors and fonts.

--- uiframework/src/org/cougaar/lib/uiframework/ui/themes/BlueTheme.java
+++ uiframework/src/org/cougaar/lib/uiframework/ui/themes/BlueTheme.java
@@ -8,41 +8,54 @@ import java.awt.*;
 
 /**
  * This class describes a theme using blue colors.
- *
- * 1.3 11/19/00
- * @author Peter Fischer
  */
 public class BlueTheme extends DefaultMetalTheme {
 
     public String getName() { return "Blue"; }
 
-    private final ColorUIResource primary1 = new ColorUIResource(0, 0, 144); // labels
-    private final ColorUIResource primary2 = new ColorUIResource(100, 100, 255);// labels, handles highlight, shadows
-    private final ColorUIResource primary3 = new ColorUIResource(100, 100, 255); // text selection
+    // Delta colors and fonts obtained from FgiPanel.java
+    public static final ColorUIResource black = new ColorUIResource(0,0,0);
+    public static final ColorUIResource white = new ColorUIResource(255,255,255);
+    public static final ColorUIResource red = new ColorUIResource(255,0,0);
+    public static final ColorUIResource green = new ColorUIResource(0, 255, 0);
+    public static final ColorUIResource blue = new ColorUIResource(0,0,255);
+    public static final ColorUIResource isiLightBlue = new ColorUIResource(202, 218, 220);
+    public static final ColorUIResource isiDarkBlue = new ColorUIResource(150, 180, 185);
+    public static final ColorUIResource isiBrown = new ColorUIResource(200,200,165);
+    public static final FontUIResource titleFont = new FontUIResource("Arial", Font.BOLD, 16);
+    public static final FontUIResource attributeNameFont = new FontUIResource("Arial", Font.BOLD, 14);
+    public static final FontUIResource largeTitleFont = new FontUIResource("Arial", Font.BOLD, 20);
+    public static final FontUIResource standardFont = new FontUIResource("Arial", Font.PLAIN, 13);
+    public static final FontUIResource largeFont = new FontUIResource("Arial", Font.PLAIN, 14);
+    public static final FontUIResource largeBoldFont = new FontUIResource("Arial", Font.BOLD, 14);
+    public static final FontUIResource smallFont = new FontUIResource("Arial", Font.BOLD, 12);
 
-    private final ColorUIResource secondary1 = new ColorUIResource(0, 0, 80); // Inactive window borders (control outlines)
-    private final ColorUIResource secondary2 = new ColorUIResource(20, 20, 120); // Secondary shadows
-    private final ColorUIResource secondary3 = new ColorUIResource(210, 210, 255);// background
+    // color I made up
+    public static final ColorUIResource foregroundBlue = new ColorUIResource(111, 149, 156);
 
-    private final ColorUIResource black = new ColorUIResource(0, 0, 0);
-    private final ColorUIResource white = new ColorUIResource(222, 222, 222);
+    protected ColorUIResource getPrimary1() { return foregroundBlue; } // labels
+    protected ColorUIResource getPrimary2() { return foregroundBlue; } // labels, handles highlight, shadows
+    protected ColorUIResource getPrimary3() { return isiBrown; } // text selection
 
-    protected ColorUIResource getPrimary1() { return primary1; }
-    protected ColorUIResource getPrimary2() { return primary2; }
-    protected ColorUIResource getPrimary3() { return primary3; }
-
-    protected ColorUIResource getSecondary1() { return secondary1; }
-    protected ColorUIResource getSecondary2() { return secondary2; }
-    protected ColorUIResource getSecondary3() { return secondary3; }
+    protected ColorUIResource getSecondary1() { return isiBrown; } // Inactive window borders (control outlines)
+    protected ColorUIResource getSecondary2() { return isiBrown; } // Secondary shadows
+    protected ColorUIResource getSecondary3() { return isiLightBlue; } // background
 
     protected ColorUIResource getBlack() { return black; }
     protected ColorUIResource getWhite() { return white; }
 
+    public FontUIResource getControlTextFont() { return attributeNameFont;}
+    public FontUIResource getSystemTextFont() { return standardFont;}
+    public FontUIResource getUserTextFont() { return standardFont;}
+    public FontUIResource getMenuTextFont() { return standardFont;}
+    public FontUIResource getWindowTitleFont() { return largeTitleFont;}
+    public FontUIResource getSubTextFont() { return smallFont;}
     public void addCustomEntriesToTable(UIDefaults table)
+
     {
-        table.put("Button.background", new ColorUIResource(0, 0, 150));
-        table.put("Button.foreground", new ColorUIResource(255, 255, 255));
-        table.put("ComboBox.background", new ColorUIResource(0, 0, 150));
-        table.put("ComboBox.foreground", new ColorUIResource(255, 255, 255));
+        table.put("Button.background", isiDarkBlue);
+        table.put("Button.foreground", white);
+        table.put("ComboBox.background", isiDarkBlue);
+        table.put("ComboBox.foreground", white);
     }
 }
