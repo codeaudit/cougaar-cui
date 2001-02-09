a9071f675ce234edd0ec5a6c90baa574c7d32311
gdonovan <gdonovan>
2001-02-09 20:50:48 +0000

    Some test configuration files for using the org location stuff.

---
+++ uiframework/configs/test/runsoc.bat
@@ -0,0 +1,22 @@
+@echo off
+
+setlocal
+
+set NODE=MyNode
+
+set EXECLASS=org.cougaar.core.society.Node
+set NODEARGS=-c -n %NODE%
+
+set BASELIB=d:\home\alpine\lib
+set CPATH=%BASELIB%\core.jar
+set CPATH=%CPATH%;%BASELIB%\glm.jar
+set CPATH=%CPATH%;%BASELIB%\planserver.jar
+set CPATH=%CPATH%;%BASELIB%\aggagent.jar
+set CPATH=%CPATH%;%BASELIB%\xalan.jar
+set CPATH=%CPATH%;%BASELIB%\xerces.jar
+set CPATH=%CPATH%;%BASELIB%\xml4j_2_0_11.jar
+set CPATH=%CPATH%;d:\cui\uiframework\classes
+
+java -cp %CPATH% %EXECLASS% %NODEARGS%
+
+@echo on
