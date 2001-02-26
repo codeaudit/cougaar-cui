d6f29da838576b8ee5d49a91048790116a1bb242
krotherm <krotherm>
2001-02-26 23:06:28 +0000

    added stoplight panel popup

--- uiframework/scripts/map/runmapui.bat
+++ uiframework/scripts/map/runmapui.bat
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 echo Running Map UI...
 
 setlocal
@@ -11,10 +11,28 @@ set LIB_PATH=%CUR_BIN_DIR%
 set LIB_PATH=%LIB_PATH%;%LIB_HOME%\xml4j_2_0_11.jar
 set LIB_PATH=%LIB_PATH%;%LIB_HOME%\core.jar
 set LIB_PATH=%LIB_PATH%;%LIB_HOME%\openmap.jar
+set LIB_PATH=%LIB_PATH%;%LIB_HOME%\classes122.zip
 set LIB_PATH=%LIB_PATH%;.\data
 
+
+
+set DBTYPE="oracle"
+rem set DBTYPE="access"
+
+rem set DBURL="quicktableDemo"
+set DBURL="alp-demo:1521:alp"
+rem set DBURL="eiger.alpine.bbn.com:1521:alp"
+
+set DBUSER="pfischer"
+rem set DBUSER="jmeyer"
+rem set DBUSER="blackjack8"
+
+set DBPASSWORD="pfischer"
+rem set DBPASSWORD="jmeyer"
+rem set DBPASSWORD="init1389"
+
 set JAVAC=c:\jdk1.2.2\bin\java
-set JAVAFLAGS=  -mx64m  %Debugging% -Dcmap.configDir=.\data -classpath %LIB_PATH%
+set JAVAFLAGS=  -mx64m  %Debugging% -Dcmap.configDir=.\data  -DDBTYPE=%DBTYPE% -DDBURL=%DBURL% -DDBUSER=%DBUSER% -DDBPASSWORD=%DBPASSWORD% -classpath %LIB_PATH%
 
 %JAVAC% %JAVAFLAGS% org.cougaar.lib.uiframework.ui.map.app.CMap
 
