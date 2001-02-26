be2ec4e2f6017107a16fc63827dab4065e414e40
krotherm <krotherm>
2001-02-26 15:33:04 +0000

    Changed our OpenMap application class name to CMap

--- uiframework/scripts/map/runmapui.bat
+++ uiframework/scripts/map/runmapui.bat
@@ -14,9 +14,9 @@ set LIB_PATH=%LIB_PATH%;%LIB_HOME%\openmap.jar
 set LIB_PATH=%LIB_PATH%;.\data
 
 set JAVAC=c:\jdk1.2.2\bin\java
-set JAVAFLAGS=  -mx64m  %Debugging% -Dopenmap.configDir=.\data -classpath %LIB_PATH%
+set JAVAFLAGS=  -mx64m  %Debugging% -Dcmap.configDir=.\data -classpath %LIB_PATH%
 
-%JAVAC% %JAVAFLAGS% org.cougaar.lib.uiframework.ui.map.app.OpenMap
+%JAVAC% %JAVAFLAGS% org.cougaar.lib.uiframework.ui.map.app.CMap
 
 echo Finished Running Map UI.
 echo on
