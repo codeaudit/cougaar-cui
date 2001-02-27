910afb762f4bd5f19b9ce5e2ecc4e35db58b571f
gdonovan <gdonovan>
2001-02-27 16:13:34 +0000

    Expanded the build script to build all of the classes.

--- uiframework/build.bat
+++ uiframework/build.bat
@@ -6,36 +6,74 @@ set SP=.\src
 
 set OP=.\classes
 
-set LIB=e:\DELTAInst\lib
+rem *** alp-demo **** set LIB=e:\DELTAInst\lib
+rem *** TIC **** set LIB=.\lib
+rem the following is for when the build.bat is in uiframework directory
+set LIB=.\lib
 
 set CP=
+set CP=%CP%;%LIB%\xerces.jar
+set CP=%CP%;%LIB%\vgj.jar
 set CP=%CP%;%LIB%\aggagent.jar
 set CP=%CP%;%LIB%\core.jar
 set CP=%CP%;%LIB%\glm.jar
 set CP=%CP%;%LIB%\planserver.jar
 set CP=%CP%;%LIB%\xml4j_2_0_11.jar
-set CP=%CP%;e:\j2sdkee1.2.1\lib\j2ee.jar
+set CP=%CP%;%LIB%\j2ee.jar
+set CP=%CP%;%LIB%\xerces.jar
+set CP=%CP%;%LIB%\vgj.jar
+set CP=%CP%;%LIB%\xerces.jar
+set CP=%CP%;%LIB%\vgj.jar
+set CP=%CP%;%LIB%\cuimap.jar
 
 set SRCR=src\org\cougaar\lib\uiframework
 set SRBJ=src\mil\darpa\log\alpine\blackjack
 
-set JF=
-set JF=%JF% %SRCR%\query\*.java
-set JF=%JF% %SRCR%\query\generic\*.java
-set JF=%JF% %SRCR%\query\test\*.java
-set JF=%JF% %SRCR%\transducer\*.java
-set JF=%JF% %SRCR%\transducer\configs\*.java
-set JF=%JF% %SRCR%\transducer\elements\*.java
-set JF=%JF% %SRCR%\transducer\dbsupport\*.java
-set JF=%JF% %SRCR%\ui\orglocation\data\*.java
-set JF=%JF% %SRCR%\ui\orglocation\plugin\*.java
-set JF=%JF% %SRCR%\ui\orglocation\psp\*.java
-set JF=%JF% %SRCR%\ui\orglocation\psp\xmlservice\*.java
-set JF=%JF% %SRBJ%\assessui\middletier\*.java
-set JF=%JF% %SRBJ%\assessui\client\*.java
-set JF=%JF% %SRBJ%\assessui\util\*.java
-set JF=%JF% %SRBJ%\assessui\society\*.java
+set JF1=
+set JF2=
+set JF3=
 
-javac -classpath %CP% -sourcepath %SP% -d %OP% %JF%
+set JF1=%JF1% %SRCR%\query\*.java
+set JF1=%JF1% %SRCR%\query\generic\*.java
+set JF1=%JF1% %SRCR%\query\test\*.java
+set JF1=%JF1% %SRCR%\transducer\*.java
+set JF1=%JF1% %SRCR%\transducer\configs\*.java
+set JF1=%JF1% %SRCR%\transducer\elements\*.java
+set JF1=%JF1% %SRCR%\transducer\dbsupport\*.java
+set JF1=%JF1% %SRCR%\ui\orglocation\data\*.java
+set JF1=%JF1% %SRCR%\ui\orglocation\plugin\*.java
+set JF1=%JF1% %SRCR%\ui\orglocation\psp\*.java
+set JF1=%JF1% %SRCR%\ui\orglocation\psp\xmlservice\*.java
+set JF1=%JF1% %SRCR%\ui\orgui\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\tree\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\cgd\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\cartegw\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\shawn\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\examplealg\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\gui\*.java 
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\graph\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\VGJ\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\*.java
+set JF2=%JF2% %SRCR%\ui\ohv\util\*.java
+set JF2=%JF2% %SRCR%\ui\components\*.java
+set JF2=%JF2% %SRCR%\ui\components\graph\*.java
+set JF2=%JF2% %SRCR%\ui\components\mthumbslider\*.java
+set JF3=%JF3% %SRCR%\ui\map\app\*.java
+set JF3=%JF3% %SRCR%\ui\map\layer\*.java
+set JF3=%JF3% %SRCR%\ui\map\util\*.java
+set JF3=%JF3% %SRCR%\ui\models\*.java
+set JF3=%JF3% %SRCR%\ui\themes\*.java
+set JF3=%JF3% %SRCR%\ui\util\*.java
+
+set JF3=%JF3% %SRBJ%\assessui\middletier\*.java
+set JF3=%JF3% %SRBJ%\assessui\webtier\*.java
+set JF3=%JF3% %SRBJ%\assessui\client\*.java
+set JF3=%JF3% %SRBJ%\assessui\util\*.java
+set JF3=%JF3% %SRBJ%\assessui\society\*.java
+
+javac -classpath %CP% -sourcepath %SP% -d %OP% %JF1%
+javac -classpath %CP% -sourcepath %SP% -d %OP% %JF2%
+javac -classpath %CP% -sourcepath %SP% -d %OP% %JF3%
 
 @echo on
