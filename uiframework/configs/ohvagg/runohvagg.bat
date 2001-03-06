868e7cf123c57c49ee13d59aab41e5bd49368923
gdonovan <gdonovan>
2001-03-06 16:37:09 +0000

    Initial check-in for OHV's the AggAgent configuration files

---
+++ uiframework/configs/ohvagg/runohvagg.bat
@@ -0,0 +1,22 @@
+@echo off
+
+setlocal
+
+set NODE=OhvAggNode
+
+set EXECLASS=org.cougaar.core.society.Node
+set NODEARGS=-c -n %NODE%
+
+set BASELIB=s:\alp70\alp\lib
+set CPATH=%BASELIB%\core.jar
+set CPATH=%CPATH%;%BASELIB%\glm.jar
+set CPATH=%CPATH%;%BASELIB%\planserver.jar
+set CPATH=%CPATH%;%BASELIB%\aggagent.jar
+set CPATH=%CPATH%;%BASELIB%\xalan.jar
+set CPATH=%CPATH%;%BASELIB%\xerces.jar
+set CPATH=%CPATH%;%BASELIB%\xml4j_2_0_11.jar
+set CPATH=%CPATH%;..\..\classes
+
+java -cp %CPATH% %EXECLASS% %NODEARGS%
+
+@echo on
