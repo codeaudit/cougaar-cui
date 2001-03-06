a3598f7feba52a48ff58ce11f22be2fbae427395
gdonovan <gdonovan>
2001-03-06 14:36:27 +0000

    Modified to build without errors resulting from rogue xerces jars

--- uiframework/build.bat
+++ uiframework/build.bat
@@ -9,7 +9,7 @@ set OP=.\classes
 rem *** alp-demo **** set LIB=e:\DELTAInst\lib
 rem *** TIC **** set LIB=.\lib
 rem the following is for when the build.bat is in uiframework directory
-set LIB=.\lib
+set LIB=..\..\lib
 
 set CP=
 set CP=%CP%;%LIB%\xerces.jar
@@ -26,9 +26,15 @@ set CP=%CP%;%LIB%\xerces.jar
 set CP=%CP%;%LIB%\vgj.jar
 set CP=%CP%;%LIB%\cuimap.jar
 
+set CP0=%OP%
+set CP0=%CP0%;%LIB%\xerces.jar
+set CP0=%CP0%;%LIB%\vgj.jar
+set CP0=%CP0%;%LIB%\core.jar
+
 set SRCR=src\org\cougaar\lib\uiframework
 set SRBJ=src\mil\darpa\log\alpine\blackjack
 
+set JF0=
 set JF1=
 set JF2=
 set JF3=
@@ -45,23 +51,27 @@ set JF1=%JF1% %SRCR%\ui\orglocation\plugin\*.java
 set JF1=%JF1% %SRCR%\ui\orglocation\psp\*.java
 set JF1=%JF1% %SRCR%\ui\orglocation\psp\xmlservice\*.java
 set JF1=%JF1% %SRCR%\ui\orgui\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\tree\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\cgd\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\cartegw\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\algorithm\shawn\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\examplealg\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\gui\*.java 
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\graph\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\VGJ\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\*.java
-set JF2=%JF2% %SRCR%\ui\ohv\util\*.java
+
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\algorithm\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\algorithm\tree\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\algorithm\cgd\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\algorithm\cartegw\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\algorithm\shawn\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\examplealg\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\gui\*.java 
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\graph\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\VGJ\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\*.java
+set JF0=%JF0% %SRCR%\ui\ohv\util\*.java
+
 set JF2=%JF2% %SRCR%\ui\components\*.java
 set JF2=%JF2% %SRCR%\ui\components\graph\*.java
 set JF2=%JF2% %SRCR%\ui\components\mthumbslider\*.java
+
 set JF3=%JF3% %SRCR%\ui\map\app\*.java
 set JF3=%JF3% %SRCR%\ui\map\layer\*.java
 set JF3=%JF3% %SRCR%\ui\map\util\*.java
+set JF3=%JF3% %SRCR%\ui\map\query\*.java
 set JF3=%JF3% %SRCR%\ui\models\*.java
 set JF3=%JF3% %SRCR%\ui\themes\*.java
 set JF3=%JF3% %SRCR%\ui\util\*.java
@@ -72,6 +82,7 @@ set JF3=%JF3% %SRBJ%\assessui\client\*.java
 set JF3=%JF3% %SRBJ%\assessui\util\*.java
 set JF3=%JF3% %SRBJ%\assessui\society\*.java
 
+javac -classpath %CP0% -sourcepath %SP% -d %OP% %JF0%
 javac -classpath %CP% -sourcepath %SP% -d %OP% %JF1%
 javac -classpath %CP% -sourcepath %SP% -d %OP% %JF2%
 javac -classpath %CP% -sourcepath %SP% -d %OP% %JF3%
