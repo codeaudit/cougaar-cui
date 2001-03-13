637e4df447fca5bce521e2d36401a493819303f0
krotherm <krotherm>
2001-03-13 22:03:01 +0000

    updated config for TIC

--- uiframework/scripts/ohv/runohv.bat
+++ uiframework/scripts/ohv/runohv.bat
@@ -1,27 +1,38 @@
-@rem **** 
 @echo off
 
-@rem set ORGHV_HOME=c:\dev\ui\fromcvs\cougaar\uiframework
+setlocal
+
+set ORGHV_HOME=..\..
 if "%ORGHV_HOME%"=="" goto err1
 if NOT EXIST "%ORGHV_HOME%"  goto err2
 
-set COUGAAR_INSTALL_PATH=c:\alpbuild\alp7\alp-20010215
-set LIBPATH=
-set LIBPATH=%LIBPATH%;%ORGHV_HOME%\bin;%ORGHV_HOME%\config;;%ORGHV_HOME%\data
-set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%\lib\core.jar
-set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%\lib\xerces.jar
-set LIBPATH=%LIBPATH%;c:\integ\disptool\origFromZip\vgj.jar
-
 set SRC_BASE=%ORGHV_HOME%\src
-set BIN_BASE=%ORGHV_HOME%\bin
+set BIN_BASE=%ORGHV_HOME%\classes
+set LIB_BASE=%ORGHV_HOME%\..\..\lib
+rem set LIB_BASE=%COUGAAR_INSTALL_PATH%
 
+set LIBPATH=.
+set LIBPATH=%LIBPATH%;%BIN_BASE%
+set LIBPATH=%LIBPATH%;%ORGHV_HOME%\configs
+set LIBPATH=%LIBPATH%;%ORGHV_HOME%\data
+set LIBPATH=%LIBPATH%;%LIB_BASE%\core.jar
+set LIBPATH=%LIBPATH%;%LIB_BASE%\xerces.jar
+set LIBPATH=%LIBPATH%;%LIB_BASE%\vgj.jar
 
 set APP=org.cougaar.lib.uiframework.ui.ohv.OrgHierApp
 if "%1" == "editor" set APP=org.cougaar.lib.uiframework.ui.ohv.OrgHierEditorApp
 echo %APP%
-if "%2" == "" echo Usage requires parameters: URL APP or defaultTest APP where APP is testciv or testdlv
 
-set JAVA_CMD=c:\jdk1.2.2\bin\java   -Xms100m -Xmx300m  -classpath %LIBPATH%  %APP% %1 %2
+rem For class OrgHierApp:
+rem Usage requires parameters: URL APP or defaultTest APP where APP is testciv or testdlv
+rem The next-to-last word in the following command is the URL which supplies relational data
+
+rem 
+set JAVA_CMD=java -Xms100m -Xmx300m -classpath %LIBPATH%  %APP% defaultTest cview 
+rem echo testciv
+rem set JAVA_CMD=java -Xms100m -Xmx300m -classpath %LIBPATH% %APP% http://localhost:5555/$ohvagg/org/orgrels.psp  testdlv editor
+set JAVA_CMD=java -Xms100m -Xmx300m -classpath %LIBPATH% %APP% http://localhost:5555/$ohvagg/org/orgrels.psp  cview
+
 
 if NOT EXIST %BIN_BASE% echo **** Error: No directory for bytecode: %BIN_BASE%
 
@@ -40,7 +51,6 @@ goto end
  goto end
 
 :end
-popd
-echo done.
 
+echo done.
 @echo on
