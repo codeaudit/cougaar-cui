0f1fc21b59e96eb8088f6a9731586052774b2770
krotherm <krotherm>
2001-03-22 16:28:47 +0000

    Initial commit

---
+++ uiframework/src/org/cougaar/lib/uiframework/ui/map/layer/cgmicon/CGMVecIcon.java
@@ -0,0 +1,94 @@
+package org.cougaar.lib.uiframework.ui.map.layer.cgmicon;
+
+import java.awt.Graphics;
+import java.awt.Color;
+
+import org.cougaar.lib.uiframework.ui.map.layer.VecIcon;
+
+import com.bbn.openmap.proj.Projection;
+
+import com.bbn.openmap.omGraphics.OMGraphic;
+
+public class CGMVecIcon extends VecIcon
+{
+
+  OMCGM cgmIcon = null;
+
+  public CGMVecIcon()
+  {
+
+  }
+
+  public CGMVecIcon (OMCGM cgm, float lat, float lon, int sc)
+  {
+    super (lat, lon, Color.white, Color.black, sc);
+
+    cgmIcon = cgm;
+    cgmIcon.setLocation(lat, lon, OMGraphic.DECIMAL_DEGREES);
+
+  }
+
+  public CGMVecIcon (OMCGM cgm, float lat, float lon)
+  {
+     super (lat, lon, Color.white);
+     cgmIcon = cgm;
+     cgmIcon.setLocation(lat, lon, OMGraphic.DECIMAL_DEGREES);
+  }
+
+
+  public void setLocation(float latPoint, float lonPoint, int units)
+  {
+    cgmIcon.setLocation(latPoint, lonPoint, units);
+  }
+
+  public CGMVecIcon makeAnother()
+  {
+
+     try
+     {
+       CGMVecIcon newMe = new CGMVecIcon();
+
+       if (cgmIcon instanceof OMCGMbyVisio)
+       {
+          newMe.cgmIcon = new OMCGMbyVisio();
+       }
+       else
+       {
+         newMe.cgmIcon = new OMCGM();
+       }
+
+
+       if (this.cgmIcon.unitSize != null)
+         newMe.cgmIcon.unitSize = new String (this.cgmIcon.unitSize);
+       newMe.cgmIcon.cgmFileName = new String (this.cgmIcon.cgmFileName);
+       newMe.cgmIcon.omcgmdisp = (OpenMapCGMDisplay) (this.cgmIcon.omcgmdisp.makeAnother());
+
+       return newMe;
+
+     }
+
+     catch (Throwable ioexc)
+     {
+       System.err.println ("can't create new copy of CGMVecIcon: " + ioexc.toString());
+       ioexc.printStackTrace();
+     }
+
+     return null;
+
+   }
+
+   public boolean generate (Projection proj)
+   {
+     return super.generate(proj) & cgmIcon.generate(proj) ;
+   }
+
+  public void render (Graphics g)
+  {
+    super.render(g);
+    cgmIcon.render(g);
+  }
+
+    protected void initSymbol() {}
+    protected void initBoundingBox() {}
+  
+}
