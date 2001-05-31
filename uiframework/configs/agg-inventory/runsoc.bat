d0f517c6c425bc943abdb58a6c4a06490ce57d6e
jmeyer <jmeyer>
2001-05-31 20:57:02 +0000

    Merging with the head

---
+++ uiframework/configs/agg-inventory/runsoc.bat
@@ -0,0 +1,22 @@
+@echo off
+
+setlocal
+
+set NODE=AggInventoryNode
+
+set EXECLASS=org.cougaar.core.society.Node
+set NODEARGS=-c -n %NODE%
+
+set BASELIB=%ALP_INSTALL_PATH%\lib
+set CPATH=%BASELIB%\core.jar
+set CPATH=%CPATH%;%BASELIB%\glm.jar
+set CPATH=%CPATH%;%BASELIB%\planserver.jar
+set CPATH=%CPATH%;%BASELIB%\aggagent.jar
+set CPATH=%CPATH%;%BASELIB%\xalan.jar
+set CPATH=%CPATH%;%BASELIB%\xerces.jar
+set CPATH=%CPATH%;%BASELIB%\xml4j_2_0_11.jar
+set CPATH=%CPATH%;%BASELIB%\uiframework.jar
+
+java -Xmx128M -cp %CPATH% %EXECLASS% %NODEARGS%
+
+@echo on
