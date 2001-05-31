f98270f440fd7ae330ee2207d88d697415047075
jmeyer <jmeyer>
2001-05-31 20:45:56 +0000

    Merging with the head

--- uiframework/configs/agg-demand/runsoc.bat
+++ uiframework/configs/agg-demand/runsoc.bat
@@ -2,12 +2,12 @@
 
 setlocal
 
-set NODE=MyNode
+set NODE=AggDemandNode
 
 set EXECLASS=org.cougaar.core.society.Node
 set NODEARGS=-c -n %NODE%
 
-set BASELIB=..\..\lib
+set BASELIB=%ALP_INSTALL_PATH%\lib
 set CPATH=%BASELIB%\core.jar
 set CPATH=%CPATH%;%BASELIB%\glm.jar
 set CPATH=%CPATH%;%BASELIB%\planserver.jar
@@ -15,8 +15,8 @@ set CPATH=%CPATH%;%BASELIB%\aggagent.jar
 set CPATH=%CPATH%;%BASELIB%\xalan.jar
 set CPATH=%CPATH%;%BASELIB%\xerces.jar
 set CPATH=%CPATH%;%BASELIB%\xml4j_2_0_11.jar
-set CPATH=%CPATH%;..\..\classes
+set CPATH=%CPATH%;%BASELIB%\uiframework.jar
 
-java -cp %CPATH% %EXECLASS% %NODEARGS%
+java -Xmx128M -cp %CPATH% %EXECLASS% %NODEARGS%
 
 @echo on
