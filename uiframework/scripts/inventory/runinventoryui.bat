7c938730d284ace7535db431c5bdac6810ba7652
mdavis <mdavis>
2001-06-14 18:47:57 +0000

    Scripts to run the pat clark gui

--- uiframework/scripts/inventory/runinventoryui.bat
+++ uiframework/scripts/inventory/runinventoryui.bat
@@ -13,5 +13,5 @@ rem set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\classes
 
 @ECHO ON
 
-java -Djava.compiler=NONE -classpath %LIBPATHS% org.cougaar.lib.uiframework.ui.components.CChart
-pause
+java -Djava.compiler=NONE -classpath %LIBPATHS% org.cougaar.lib.uiframework.ui.inventory.InventoryChartUI l 4
+REM java -classpath %LIBPATHS% org.cougaar.lib.uiframework.ui.inventory.InventoryChartUI l 4
