2403b5a60665af98dcefcf33ecc67528d7a60d5f
mdavis <mdavis>
2001-06-25 19:13:15 +0000

    Modified to pickup the ScaleUp and ScaleDown gifs.

--- uiframework/bin/CUI-Map.bat
+++ uiframework/bin/CUI-Map.bat
@@ -1,9 +1,13 @@
 @ECHO OFF
 echo Running Map UI...
 
-set CONFIG_DIR=.\data
+set CONFIG_DIR=..\data
 
 set LIBPATHS=.
+
+set LIBPATHS=%LIBPATHS%;..\src
+set LIBPATHS=%LIBPATHS%;..\src\org\cougaar\lib\uiframework\ui\components
+
 REM set LIBPATHS=%LIBPATHS%;.\mysql.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\mm-mysql-2.jar
 
@@ -13,6 +17,7 @@ set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\cuimap.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\glm.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\tops.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\uiframework.jar
+REM set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\classes
 
 set LIBPATHS=%LIBPATHS%;%CONFIG_DIR%
 
