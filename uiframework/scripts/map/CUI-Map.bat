1c702e767484a2b7f30c62dc5aee6c19a4114a7b
mdavis <mdavis>
2001-06-25 18:58:20 +0000

    Temporary update of scripts files for map module from Pat Clark for 8.2 version of uiframework.  scripts directory should be eventually deleted.

---
+++ uiframework/scripts/map/CUI-Map.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+echo Running Map UI...
+
+set CONFIG_DIR=.\data
+
+set LIBPATHS=.
+REM set LIBPATHS=%LIBPATHS%;.\mysql.jar
+set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\mm-mysql-2.jar
+
+set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\xml4j_2_0_11.jar
+set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\core.jar
+set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\cuimap.jar
+set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\glm.jar
+set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\tops.jar
+set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\uiframework.jar
+
+set LIBPATHS=%LIBPATHS%;%CONFIG_DIR%
+
+set JAVAFLAGS=-mx64m -DScenarioMap.configDir=%CONFIG_DIR% -DDBTYPE=%DBTYPE% -DDBURL=%DBURL% -DDBUSER=%DBUSER% -DDBPASSWORD=%DBPASSWORD%
+
+@ECHO ON
+
+java %JAVAFLAGS% -Djava.compiler=NONE -classpath %LIBPATHS% org.cougaar.lib.uiframework.ui.map.app.ScenarioMap
+
+echo Finished Running Map UI.
