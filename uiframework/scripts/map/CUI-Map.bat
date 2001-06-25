c6385864bd8468169d7c8d33b7d8d919f0d349f9
mdavis <mdavis>
2001-06-25 19:12:05 +0000

    BAT file modified to include scaleUp.gif and scaleDown.gif.

--- uiframework/scripts/map/CUI-Map.bat
+++ uiframework/scripts/map/CUI-Map.bat
@@ -7,12 +7,17 @@ set LIBPATHS=.
 REM set LIBPATHS=%LIBPATHS%;.\mysql.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\mm-mysql-2.jar
 
+set LIBPATHS=%LIBPATHS%;..\..\src
+set LIBPATHS=%LIBPATHS%;..\..\src\org\cougaar\lib\uiframework\ui\components
+
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\xml4j_2_0_11.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\core.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\cuimap.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\glm.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\tops.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\uiframework.jar
+
+REM set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\classes
 
 set LIBPATHS=%LIBPATHS%;%CONFIG_DIR%
 
