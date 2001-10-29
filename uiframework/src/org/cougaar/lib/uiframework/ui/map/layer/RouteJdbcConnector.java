e941995b1dcdc463e05f71fd8bcf169ace85690e
mdavis <mdavis>
2001-10-29 20:51:22 +0000

    Got rid of compile time dependence on mysql.   should fix compilation error.

--- uiframework/src/org/cougaar/lib/uiframework/ui/map/layer/RouteJdbcConnector.java
+++ uiframework/src/org/cougaar/lib/uiframework/ui/map/layer/RouteJdbcConnector.java
@@ -1,10 +1,21 @@
 /*
  * <copyright>
- * Copyright 1997-2000 Defense Advanced Research Projects Agency (DARPA)
- * and Clark Software Engineering (CSE) This software to be used in
- * accordance with the COUGAAR license agreement.  The license agreement
- * and other information on the Cognitive Agent Architecture (COUGAAR)
- * Project can be found at http://www.cougaar.org or email: info@cougaar.org.
+ *  Copyright 1997-2001 Clark Software Engineering (CSE) 
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
  * </copyright>
  */
 package org.cougaar.lib.uiframework.ui.map.layer;
@@ -14,8 +25,6 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.*;
-
-import org.gjt.mm.mysql.*;
 
 import com.bbn.openmap.LatLonPoint;
 import com.bbn.openmap.proj.Projection;
@@ -97,7 +106,8 @@ public class RouteJdbcConnector
 
     try
     {
-       DriverManager.registerDriver(new org.gjt.mm.mysql.Driver());
+       Driver myDriver = (Driver) Class.forName("org.gjt.mm.mysql.Driver()").newInstance();
+       DriverManager.registerDriver(myDriver);
 
        System.out.println("Connecting to the datasource : " );
        conn = DriverManager.getConnection("jdbc:mysql://localhost/RouteDB");
