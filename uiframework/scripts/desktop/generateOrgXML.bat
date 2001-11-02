ddf3c763e339de202acf84282991a5ffac5e0fbd
pfischer <pfischer>
2001-11-02 13:49:36 +0000

    Updated CLASSPATH to use latest jars.

--- uiframework/scripts/desktop/generateOrgXML.bat
+++ uiframework/scripts/desktop/generateOrgXML.bat
@@ -25,9 +25,11 @@ echo * To use, first place xml4j_2_0_11.jar (IMB XML parser)     *
 echo * in ..\..\lib.                                             *
 echo *************************************************************
 
-set LIB_PATH=..\..\lib
+set LIB_PATH=c:\alpine\lib
+rem set LIB_PATH=..\..\lib
 
 set CP=..\..\classes
-set CP=%CP%;%LIB_PATH%\xml4j_2_0_11.jar
+set CP=%CP%;%LIB_PATH%\xerces.jar
+set CP=%CP%;%LIB_PATH%\blackjack.jar
 
 java -classpath %CP% mil.darpa.log.alpine.blackjack.assessui.util.OrgXMLGenerator %1
