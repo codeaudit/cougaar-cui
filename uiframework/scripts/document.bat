bea70d8306eb7a72250e53cedf9018d307287308
mbarger <mbarger>
2004-01-29 22:25:55 +0000

    remove reference to code that's no longer here.  Fixes bug 3433.

--- uiframework/scripts/document.bat
+++ uiframework/scripts/document.bat
@@ -25,4 +25,4 @@ echo off
 if NOT EXIST %DSTPATH% mkdir %DSTPATH%
 echo on
 
-javadoc -package -author -version -sourcepath %SRCPATH% -d %DSTPATH% org.cougaar.lib.uiframework.ui.components org.cougaar.lib.uiframework.ui.models org.cougaar.lib.uiframework.ui.util org.cougaar.lib.uiframework.ui.themes org.cougaar.lib.uiframework.ui.components.graph org.cougaar.lib.uiframework.ui.components.mthumbslider  org.cougaar.lib.uiframework.transducer org.cougaar.lib.uiframework.transducer.configs org.cougaar.lib.uiframework.transducer.dbsupport org.cougaar.lib.uiframework.transducer.elements  mil.darpa.log.alpine.blackjack.assessui.client
+javadoc -package -author -version -sourcepath %SRCPATH% -d %DSTPATH% org.cougaar.lib.uiframework.ui.components org.cougaar.lib.uiframework.ui.models org.cougaar.lib.uiframework.ui.util org.cougaar.lib.uiframework.ui.themes org.cougaar.lib.uiframework.ui.components.graph org.cougaar.lib.uiframework.ui.components.mthumbslider  org.cougaar.lib.uiframework.transducer org.cougaar.lib.uiframework.transducer.configs org.cougaar.lib.uiframework.transducer.dbsupport org.cougaar.lib.uiframework.transducer.elements
